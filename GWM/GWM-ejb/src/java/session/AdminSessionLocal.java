065c56f47f77a010234e1c2806eb7b9220e55e18
Delven <e0407388@u.nus.edu>
2021-09-24 13:42:02 +0800

    Create Admin and AdminSession

---
+++ GWM/GWM-ejb/src/java/session/AdminSessionLocal.java
@@ -0,0 +1,24 @@
+package session;
+
+import entity.Admin;
+import error.InvalidLoginException;
+import javax.ejb.Local;
+import javax.persistence.NoResultException;
+
+@Local
+public interface AdminSessionLocal {
+    
+    public Admin getAdmin(Long userId) throws NoResultException;
+
+    public Admin getAdminByEmail(String email) throws NoResultException;
+    
+    public Long loginAdmin(String email, String password) throws InvalidLoginException;
+    
+    public void createAdmin(Admin admin);
+    
+    public void updateAdmin(Admin admin) throws NoResultException;
+}
+
+    
+    
+    
