a0228e1aff6ce421fd55922a4a80ae081ac46d7b
hongpeigoh <hong_pei_99@hotmail.com>
2021-10-09 20:59:17 +0800

    Initialize

--- GWM/gwm-react/src/App.js
+++ GWM/gwm-react/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Switch, Route, BrowserRouter } from 'react-router-dom';
+import { Button } from "atomize";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import logo from './logo.svg';
+
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="bootstrap-wrapper"
+            id="bootstrap-wrapper"
+            onKeyDown={this.onKeyPressed}
+            tabIndex="0">
+              <h1>hello world</h1>
+        </div>
+      </BrowserRouter>
+    )
+  }
 }
 
 export default App;
