84f5de85f86a53b046d09c68620315c8f5233726
wongpengyu <e0407388@u.nus.edu>
2021-10-26 14:03:56 +0800

    Update users.js
    Added: Search and Ban/Unban
    to complete: Click on row to display user profile popover/page

--- GWM/gwm-react/src/components/users.js
+++ GWM/gwm-react/src/components/users.js
@@ -1,38 +1,111 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import Box from '@mui/material/Box';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
+import { Box, IconButton, Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, TextField } from '@mui/material';
+import Search from '@mui/icons-material/Search';
+
+
 
 export function Users() {
+    const [query, setQuery] = React.useState("");
     const [users, setUsers] = React.useState([]);
 
-    try {
-        fetch(`http://localhost:8080/Gwm-war/webresources/users`, {
-            crossDomain: true
-        })
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw new Error('Something went wrong');
-                }
+    useEffect(() => {
+        handleSubmit();
+    }, [users]);
+
+    const handleKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            handleSubmit();
+        }
+    }
+
+    const handleSubmit = () => {
+        try {
+            console.log(query);
+            fetch(`http://localhost:8080/Gwm-war/webresources/users/query?name=${query}`, {
+                crossDomain: true
             })
-            .then((data) => {
-                setUsers(data);
-            });
-    } catch (e) {
-        console.log(e);
+                .then((response) => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        throw new Error('Please enter username as search condition');
+                    }
+                })
+                .then((data) => {
+                    setUsers(data);
+                });
+        } catch (e) {
+            console.log(e);
+        }
+    }
+
+    function BanUnban(props) {
+        const { userId, isAvailable } = props.user
+
+        function banUser(userId) {
+            const requestOptions = {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ title: 'Ban User' })
+            };
+            fetch(`http://localhost:8080/Gwm-war/webresources/admin/ban/${userId}`, requestOptions)
+                .then(response => response.json())
+                
+        }
+
+        function unbanUser(userId) {
+            const requestOptions = {
+                method: 'PUT',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ title: 'Ban User' })
+            };
+            fetch(`http://localhost:8080/Gwm-war/webresources/admin/unban/${userId}`, requestOptions)
+                .then(response => response.json())
+                .then((data) => {
+                    setUsers(data);
+                });
+        }
+
+        if (isAvailable)
+            return (
+                <button
+                    variant="primary"
+                    onClick={() => {
+                        const confirmBox = window.confirm(
+                            "Are you sure you want to ban this user?"
+                        )
+                        if (confirmBox === true) {
+                            banUser(userId);
+                        }
+                    }}> Ban
+                </button>
+            )
+        else {
+            return (
+                <button
+                    variant="priamry"
+                    onClick={() => {
+                        const confirmBox = window.confirm(
+                            "Are you sure you want to unban this user?"
+                        )
+                        if (confirmBox === true) {
+                            unbanUser(userId);
+                        }
+                    }}> Unban
+                </button>
+            )
+        }
     }
 
     return (
         <Box sx={{ bgcolor: '#e3f2fd', minHeight: '70vh' }}>
             <div className="container">
+                <TextField id="outlined-basic" label="Enter username to serch for users" variant="filled" value={query}
+                    onChange={(e) => setQuery(e.target.value)} onKeyDown={handleKeyDown} sx={{ minWidth: '60%' }} />
+                <IconButton color="secondary" component="span" onClick={handleSubmit} sx={{ height: '60px', width: '60px' }}>
+                    <Search />
+                </IconButton>
                 <TableContainer component={Paper}>
                     <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
                         <TableHead>
@@ -41,6 +114,7 @@ export function Users() {
                                 <TableCell align="right">Username</TableCell>
                                 <TableCell align="right">Email Address</TableCell>
                                 <TableCell align="right">Wallet Amount</TableCell>
+                                <TableCell align="right">Ban / Unban</TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
@@ -52,9 +126,10 @@ export function Users() {
                                     <TableCell component="th" scope="row">
                                         {user.userId}
                                     </TableCell>
-                                    <TableCell align="right">{user.username}</TableCell>
+                                    <TableCell align="right">{user.username} (popover to display user profile)</TableCell>
                                     <TableCell align="right">{user.email}</TableCell>
                                     <TableCell align="right">{user.wallet}</TableCell>
+                                    <TableCell align="right"><BanUnban user={user} /></TableCell>
                                 </TableRow>
                             ))}
                         </TableBody>
